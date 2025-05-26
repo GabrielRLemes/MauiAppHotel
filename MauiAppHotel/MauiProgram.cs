f358cf112b1f684a39f91eb8525dc3f9e318dc26
GabrielRLemes <gabriel.rodrigueslemes@gmail.com>
2025-05-26 10:34:13 -0300

    Add recursos ao aplicativo

--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,6 +13,8 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Gobold-Light.otf", "GoboldLight");
+                    fonts.AddFont("Gobold-Regular.otf", "GoboldRegular");
                 });
 
 #if DEBUG
